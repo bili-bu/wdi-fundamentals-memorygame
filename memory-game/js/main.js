c3c81499502815143632083e632b217928ffb596
Lilibinks <lilibinks@dmitrys-macbook.mynet>
2020-01-13 19:48:28 +0000

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -3,21 +3,27 @@
 let cards = ["queen", "queen", "king", "king"]
 let cardsInPlay = [];
 
-var cardOne = cards[2];
-cardsInPlay.push(cardOne);
-console.log('User flipped queen!');
-
-var cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-console.log('User flipped king!');
-
-if (cardsInPlay.length === 2) {
-	console.log('Two cards played');
-} 
-
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match :)");
-} else {
-	alert("Try again :(");
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match :)");
+	} else {
+		console.log("Try again :(");
+	};
 };
+
+function flipCard(cardID) {
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	};
+};
+
+flipCard(0);
+flipCard(2);
+
+	//	if (cardsInPlay.length === 2) {
+	//	console.log('Two cards played');
+	//} 
+
 
