aaf0d2ea359b017aff53cbd555c3a06a95ca8acb
Lilibinks <lilibinks@dmitrys-macbook.mynet>
2020-01-13 16:28:42 +0000

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,9 +1,23 @@
-console.log("Up and running!");
+//console.log("Up and running!");
 
-let cardOne = "king";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "queen";
+let cards = ["queen", "queen", "king", "king"]
+let cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cardOne = cards[2];
+cardsInPlay.push(cardOne);
+console.log('User flipped queen!');
+
+var cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
+console.log('User flipped king!');
+
+if (cardsInPlay.length === 2) {
+	console.log('Two cards played');
+} 
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match :)");
+} else {
+	alert("Try again :(");
+};
+
